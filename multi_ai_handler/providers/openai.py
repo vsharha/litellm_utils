854cda43e0d06f4c842b9f045ecc7dbbfaf0f301
Viktor Sharha <vsharha07@gmail.com>
2025-11-23 12:25:07 +0000

    add local flag for OpenAIProvider to override local flag in generate method

--- multi_ai_handler/providers/openai.py
+++ multi_ai_handler/providers/openai.py
@@ -8,8 +8,9 @@ from pathlib import Path
 from multi_ai_handler.generate_payload import generate_openai_payload
 
 class OpenAIProvider(AIProvider):
-    def __init__(self, base_url: str | None=None, api_key: str | None=None) -> None:
+    def __init__(self, base_url: str | None=None, api_key: str | None=None, local: bool=False) -> None:
         super().__init__()
+        self.local = local
         if api_key is None:
             api_key = os.getenv("OPENAI_API_KEY")
         self.client = OpenAI(
@@ -18,6 +19,9 @@ class OpenAIProvider(AIProvider):
         )
 
     def generate(self, system_prompt: str, user_text: str=None, file: str | Path | dict | None=None, model:str=None, temperature: float=0.0, local: bool=False) -> str:
+        if self.local:
+            local = True
+
         messages: list = generate_openai_payload(user_text, system_prompt, file, local=local)
 
         completion = self.client.chat.completions.create(
