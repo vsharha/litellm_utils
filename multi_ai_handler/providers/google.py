99b13c45e7767943e6bc0b624a33b018acbaa110
Viktor Sharha <vsharha07@gmail.com>
2025-11-23 02:03:36 +0000

    refactor google api

--- multi_ai_handler/providers/google.py
+++ multi_ai_handler/providers/google.py
@@ -1,6 +1,6 @@
-import google.generativeai as genai
+from google import genai
+from google.genai import types
 from dotenv import load_dotenv
-import os
 from pathlib import Path
 
 from multi_ai_handler.ai_provider import AIProvider
@@ -9,20 +9,20 @@ from multi_ai_handler.generate_payload import generate_google_payload
 load_dotenv()
 
 class GoogleProvider(AIProvider):
+    def __init__(self):
+        super().__init__()
+        self.client = genai.Client()
+
     def generate(self, system_prompt: str, user_text: str=None, file: str | Path | dict | None=None, model:str=None, temperature: float=0.0, link:str | None=None, api_key:str | None = None) -> str:
-        GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
-        genai.configure(api_key=GEMINI_API_KEY)
-
-        generative_model = genai.GenerativeModel(model, system_instruction=system_prompt)
-
         payload: list = generate_google_payload(user_text, file)
 
-        response = generative_model.generate_content(
+        response = self.client.models.generate_content(
+            model=model,
             contents=payload,
-            generation_config=genai.GenerationConfig(
-                temperature=temperature,
-            ),
+            config=types.GenerateContentConfig(
+                system_instruction=system_prompt,
+                temperature=temperature
+            )
         )
 
         return response.text
